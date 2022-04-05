dfc78620852503c2194bfd87952e9ac47b3918e0
Fantomas <Fantomas@pochta.ua>
2022-04-05 17:39:16 +0300

    update homework_lesson_6.py

--- homework_lesson_6.py
+++ homework_lesson_6.py
@@ -8,6 +8,9 @@ for symbol in my_list:
     if symbol > 100:
         print(symbol)
 
+#в одну строку
+# [print(symbol) for symbol in my_list if symbol > 100]
+
 # 2) У вас есть список my_list с значениями типа int, и пустой список my_results.
 # Добавить в my_results те значения, которые больше 100.
 # Распечатать список my_results.
@@ -19,6 +22,9 @@ for symbol in my_list:
     if symbol > 100:
         my_results.append(symbol)
 print(my_results)
+#в одну строку
+# print([symbol for symbol in my_list if symbol > 100])
+
 
 # 3) У вас есть список my_list с значениями типа int.
 # Если в my_list количество элементов меньше 2, то в конец добавить значение 0.
@@ -33,4 +39,7 @@ else:
     value_1 = my_list[list_len - 1]
     value_2 = my_list[list_len - 2]
     my_list.append(value_1 + value_2)
+
+# в одну строку
+# my_list.append(0) if len(my_list) < 2 else my_list.append(my_list[len(my_list) - 1] + my_list[len(my_list) - 2])
 print(my_list)
